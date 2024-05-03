57155a9917e7316d2f74756d939842383ffb39a6
theguyinpink <clement.carre.prevert.pro@gmail.com>
2024-05-03 17:13:53 +0200

    updated highlights

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { hightlightsSlides } from '../constants'
+import gsap from 'gsap';
 
 function VideoCarousel() {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying
+  })
+
+  const [loadedData, setloadedData] = useState([]);
+
+  const { isEnd, isLastVideo, startPlay, videoId, isPlaying} = video;
+
+  useEffect(() => {
+    if(loadedData.lenght > 3) {
+      if(!isPlaying) {
+        videoRef.current[videoId].pause
+      } else {
+        startPlay && videoRef.current[videoId].play()
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData])
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if(span[videoId]) {
+      //animate the progess of the video
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {
+
+        },
+
+        onComplete: () => {
+
+        }
+      })
+    }
+  }, [videoId, startPlay])
+
   return (
-    <div>VideoCarousel</div>
+    <>
+      <div className='flex items-center'>
+      {hightlightsSlides.map((list, i) => (
+        <div key={list.id} id="slider" className='sm:pr-20 pr-10'>
+          <div className='video-carousel_container'>
+            <div  className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+              <video
+                id="video"
+                playsInline={true}
+                preload='auto'
+                muted
+              >
+
+                <source src={list.video} type="video/mp4" />
+              </video>
+            </div>
+
+            <div className='absolute top-12 left-[5%] z-10'>
+              {list.textLists.map((text) => (
+                <p key={text} className='md:text-2xl text-xl font-medium'>
+                  {text}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+      </div>
+    </>
   )
 }
 
